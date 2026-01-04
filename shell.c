df4034ec939e639d3f8a025f0af07a9a259a9807
Alston Liu <alstonliu730@gmail.com>
2026-01-03 23:36:35 -0500

    Parse functionality and Find Operator functionality.

--- shell.c
+++ shell.c
@@ -20,132 +20,122 @@
 // max number of command sequence
 #define MAX_SEQ 8
 
+// symbols for scanner
+const char haystack[] = "()|<>";
+
 // Function declarations
-void parse_exec(char* input, int in_fd, int out_fd);
-void nullify(char **args);
-void execute(vector_t** cmd_vect, int num_commands);
+char** parse(char* input, int* size);
+void execute(int argc, char** argv);
+int find_operator(int argc, char** argv);
 void free_cmd_vect(vector_t** cmd_vect);
 
-// parses through the input string to split into different argument sequences
-void parse_exec(char* input, int in_fd, int out_fd) {
+/**
+ * Parses through the input and returns the list of tokens.
+ * 
+ * @param input user commands in a string format
+ * 
+ * @return address to the tokens
+ */
+char** parse(char* input, int* size) {
     assert(input != NULL);
 
     vector_t* tok_vect = vect_init(MAX_INPUT);  // Create a local vector for the tokens
     int num_tok = get_tokens(tok_vect, input);  // get tokens from the input
-    char **curr_tok = tok_vect->data;           // create a pointer to traverse through tokens
     
-    // create a child process
-    pid_t child = fork();
-    if (child == 0) { // in child process
-        int pipe_fd[2];
-        // traverse through the tokens
-        for(int i = 0; i < num_tok; i++) {
-            // check for output redirection and next argument
-            if (strcmp(curr_tok[i], ">") == 0 && (i + 1) < tok_vect->size && strlen(curr_tok[i+1]) > 0) {
-                // close stdout
-                close(out_fd);
+    // allocate memory for the output buffer
+    char** tokens = (char **) malloc(sizeof(char*) * num_tok);
 
-                // open file and replace stdout with new file descriptor
-                int fd = open(curr_tok[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                assert(fd == out_fd);
+    // copy tokens into the new buffer
+    for(int i = 0; i < num_tok; i++) {
+        // local variables
+        size_t length = strlen(tok_vect->data[i]);
+        char* curr = tok_vect->data[i];
+
+        // allocate memory for each string
+        tokens[i] = (char *) calloc(length, sizeof(char));
+
+        // copy string into result array
+        strncpy(tokens[i], curr, length);    
+    }
+
+    // free the token vector
+    free_vector(tok_vect);
+
+    // return the resulting array and number of tokens
+    *size = num_tok;
+    return tokens;
+}
+
+/**
+ * Returns the first occurance of the lowest-priority operator in the given command line.
+ * - ";" (lowest)
+ * - "|"
+ * - "<"
+ * - ">" (highest)
+ * - "()" (ignored)
+ * 
+ * For parenthesis, we will use a depth variable to track and ignore other operators
+ * in the statement until the next parenthesis.
+ * 
+ * @param argc the number of tokens
+ * @param argv the list of tokens 
+ * 
+ * @returns the position of the lowest-priority operator
+ * 
+ */
+int find_operator(int argc, char** argv) {
+    int depth = 0;
+    int semicolon_pos = -1, pipe_pos = -1, redir_pos = -1;
+
+    // Traverse through commands
+    for(int i = 0; i < argc; i++) {
+        // ignore non symbol inputs
+        if (strlen(argv[i]) != 1) {
+            continue;
+        }
+        
+        // get symbol character
+        char operator = argv[i][0];
+
+        // ignore all parentheses
+        if (operator == '(') {
+            depth++;
+            continue;
+        }
+
+        if (operator == ')') {
+            depth--;
+            continue;
+        }
+
+        // no parenthesis
+        if (depth == 0) {
+            // return first occurance of semicolon (lowest priority)
+            if (operator == ';' && semicolon_pos == -1) {
+                return i;
             }
-
-            // check for input redirection and next argument
-            if (strcmp(curr_tok[i], "<") == 0 && (i + 1) < tok_vect->size && strlen(curr_tok[i+1]) > 0) {
-                // close stdin
-                close(in_fd);
-
-                // open file and replace stdin with new file
-                int fd = open(curr_tok[i], O_RDONLY);
-                assert(fd == in_fd);
-            }
+            // keep first occurance of pipe
+            else if (operator == '|' && pipe_pos == -1) {
+                pipe_pos = i;
+            } 
             
-            // check if the current argument is a pipe
-            if (strcmp(curr_tok[i], "|") == 0) {
-                // create the pipe_fd
-                assert(pipe(pipe_fd) != -1);
-
-                // fork a child and execute the next command
-                // parse_exec([cmd2, arg, arg ..., pipe_fd[0], outfd)
+            // keep first occurance of redirection symbol
+            else if ((operator == '<' || operator == '>') && redir_pos == -1) {
+                redir_pos = i;
             }
         }
-    } else if (child < 0) { // error in fork
-        perror("Fork() failed");
-
-        // Free the token vector
-        free_vector(tok_vect);
-        exit(EXIT_FAILURE);
-    } else { // in parent process
-        // wait for the child to finish its process
-        wait(NULL);
-
-        // free the token vector
-        free_vector(tok_vect);
-    }    
-}
-
-// clears the given argument string array
-void nullify(char **args) {
-    for (int i = 0; i < MAX_INPUT; i++) {
-        memset(args[i], 0, sizeof(char*));
     }
-}
 
-// prepares a call to exec_pipe using the given arg string array
-void make_pipe(char** args) {
-
-}
-
-// execute a piping command
-void exec_pipe(char** left, char** right) {
-} 
-
-// prepares a call to redirect using the given arg string array
-void make_redirect(char** args) {
-
-}
-
-
-// performs a redirect on the given args and file in the given dir
-void redirect(char** args, char* filename, int dir) {
-
-}
-
-// returns the length of a given arg string array
-int arglen(char** args) {
-    char** itr = args;
-    int len = 0;
-    while (*itr != NULL) {
-        len++;
-        ++itr;
-    }
-    return len;
+    // Prioritize operators
+    return (pipe_pos != 0) ? pipe_pos : ((redir_pos != 0) ? redir_pos : -1);
 }
 
 // executes commands in the arguments
-void execute(vector_t** cmd_vect, int num_commands) {
-    assert(cmd_vect != NULL);
-
-    // go through each command
-    for (int i = 0; i < num_commands; i++) {
-        if (prepare_exec(cmd_vect[i], STDIN_FILENO, STDOUT_FILENO) != 0) { // error detected
-            // free the command vector
-            free_cmd_vect(cmd_vect);
-
-            // exit out of the program safely
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
-// frees the memory inside of the command vector
-void free_cmd_vect(vector_t** cmd_vect) {
-    for(int i = 0; i < MAX_SEQ; i++) {
-      free_vector(cmd_vect[i]); // free each vector
-    }
-
-    // free the pointer itself
-    free(cmd_vect);
+void execute(int argc, char** argv) {
+    assert(argv != NULL);
+    assert(argc >= 1);
+    
+    
 }
 
 //main method for user input to mini shell
@@ -155,20 +145,33 @@ int main(int argc, char **argv) {
 
     // keep running the shell program indefinitely
     while(1) {
-      printf("shell $ ");
+        printf("shell $ ");
 
-      // place to store input from user
-      char input[MAX_INPUT];
-      
-      // read user input from stdin
-      if (fgets(input, MAX_INPUT, stdin) == NULL) { // (CTRL-D)
-        // end program
-        printf("Bye bye.\n");
-        return 0;
-      }
+        // place to store input from user
+        char input[MAX_INPUT];
+        
+        // read user input from stdin
+        if (fgets(input, MAX_INPUT, stdin) == NULL) { // (CTRL-D)
+            // end program
+            printf("Bye bye.\n");
+            return 0;
+        }
 
-      // parse input into command sequences
-      parse_exec(input, STDIN_FILENO, STDOUT_FILENO);
+        // parse input into token
+        int nToken = 0;
+        char** tokens = parse(input, &nToken); 
+       
+        // flush out unneccessary input
+        fflush(stdin);
+
+        // execute the commands
+        execute(nToken, tokens);
+
+        // Free the memory in tokens
+        for (int i = 0; i < nToken; i++) {
+            free(tokens[i]);
+        }
+        free(tokens);
     }
 
     // should not return here (since user will exit through a command)
